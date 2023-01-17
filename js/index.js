c0dbeacf051378bb7ef2a2a4e1775523a71d97b1
Pilar Terron <pilarterronmartin@gmail.com>
2023-01-17 17:56:36 +0100

    Resolucion lab Pilar Terron

--- js/index.js
+++ js/index.js
@@ -1,7 +1,40 @@
 // Iteration 1: Names and Input
 
+let hacker1 = "Nick";
+console.log(`El nombre del hacker es ${hacker1}`);
+let hacker2 = "Chrome";
+console.log(`El nombre del navegador es ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length) {
+  console.log(
+    `El conductor ${hacker1} es el nombre mas largo, tiene ${hacker1.length} caracteres`
+  );
+} else {
+  console.log(
+    `El conductor ${hacker2} es el nombre mas largo, tiene ${hacker2.length} caracteres`
+  );
+}
 
 // Iteration 3: Loops
+
+let nameUp= "";
+let nameSpace = "";
+
+  for (let i = 0; i < hacker1.length; i++){
+   nameUp = hacker1.toUpperCase();
+    // console.log(nameUp)
+   nameSpace += nameUp[i] + " ";
+}
+console.log(nameSpace)
+
+
+
+
+let cadena = "";
+
+for(let i = hacker1.length -1; i >=0; i-- ){
+    cadena += hacker1[i]
+  }
+console.log(cadena)
